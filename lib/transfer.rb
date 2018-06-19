175afb09a5d3776ffec494b5149097be8679ca25
Ishani <ishaniamin@gmail.com>
2018-06-19 17:10:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,11 @@
 class Transfer
 
-  def initialize
+  attr_accessor :sender, :reciver, :amt
+
+  def initialize(sender, reciver, amt)
+    @sender = sender
+    @reciver = reciver
+    @amt = amt
   end
 
 end
